0a3f05255907c48690df04cc9e29005c84b58b81
ly15927086342 <952700304@qq.com>
2020-12-20 21:45:58 +0800

    style: 注释

--- LngLatToPixel.py
+++ LngLatToPixel.py
@@ -2,11 +2,11 @@
 
 from CoordTransform import LngLatTransfer
 from TileXY import WGS84_to_TILE
-from pyproj import Transformer
+# from pyproj import Transformer
 import numpy as np
 import math
 
-transformer = Transformer.from_crs(4326, 3857)
+# transformer = Transformer.from_crs(4326, 3857)
 
 def WGS84_to_Pixel(lng, lat, level):
 	[pointX,pointY] = LngLatTransfer().WGS84_to_WebMercator(lng,lat)
@@ -22,6 +22,8 @@ def lineString(arr,level,sw,ne):
 		res.append([px+256*(tx-sw[0]),256*(ne[1]-sw[1]+1)-(py+256*(ty-sw[1]))])
 	return res
 
+# multipolygon的wgs84坐标转pixel坐标
+# 公式参考http://cntchen.github.io/2016/05/09/%E5%9B%BD%E5%86%85%E4%B8%BB%E8%A6%81%E5%9C%B0%E5%9B%BE%E7%93%A6%E7%89%87%E5%9D%90%E6%A0%87%E7%B3%BB%E5%AE%9A%E4%B9%89%E5%8F%8A%E8%AE%A1%E7%AE%97%E5%8E%9F%E7%90%86/
 def RegionToPixels(multiPoly,level,sw,ne):
 	polys = []
 	for poly in multiPoly:
